c366854befbe67742ac93458199fc7a0cbd7a286
smriti12397 <78731697+smriti12397@users.noreply.github.com>
2021-10-25 00:12:08 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,5 +1,6 @@
 // C++ program for insertion sort
 //Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands.
+//complexcity: Average complexity: n^2 //
 #include <bits/stdc++.h>
 using namespace std;
 
